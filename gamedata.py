5ba74eafda921f754a9e7df033b31a26fece8d30
Nicholas H.Tollervey <ntoll@ntoll.org>
2018-04-20 18:07:35 +0100

    Finished. TODO: Package and cross platform test.

--- gamedata.py
+++ gamedata.py
@@ -1,15 +1,27 @@
+"""
+Contains definitions of the various assets used in the
+game.
+"""
+
+from pgzero.actor import Actor
+
 WIDTH = 1024  # Width of game area.
 HEIGHT = 384  # Height of game area.
+DISTANCE = 200  # The distance to win by.
+LEVEL_UP = 100  # Number of steps before making more difficult.
 speed = 20  # How fast non-player objects move.
 object_frequency = 100  # Smaller = more frequent.
+powerup_frequency = 400  # How likely a powerup will appear.
 steps = 0  # Counts the number of frames so far in the game.
-PULL = 3  # Cost of kicking
-PUSH = 3  # Benefit of flying
-FALL = 5  # Cost of hitting somthing.
+PULL = 2  # Cost of kicking
+PUSH = 2  # Benefit of flying
+FALL = 10  # Cost of hitting somthing.
 FLIGHT_TIME = 2  # Number of seconds flying.
+STARTED = False  # Has the race started..?
 END = False  # Has the game finished..?
+WARNING = False  # Flash the distance warning.
 
-objects = {
+ground_objects = {
     'small_ground': {
         'pos': [320, 320],
         'items': [
@@ -31,6 +43,9 @@ objects = {
             'motorbike',
         ],
     },
+}
+
+air_objects = {
     'small_air': {
         'pos': [48, 280],
         'items': [
@@ -51,3 +66,33 @@ objects = {
 }
 
 active_objects = []  # Non-player objects to avoid.
+power_up = None  # Represents am antigravity power-up in the game world.
+
+
+red = Actor('red_run1')  # Represents "Red"
+red.name = 'red'  # Used to select which colour animation to use.
+red.pos = (512, 304)  # Start position
+red.frame = 1  # Start frame for the animations.
+red.jumping = False  # Jumping state.
+red.flying = False  # Flying state.
+red.kicking = False  # Kicking state.
+red.landing = False  # Landing state.
+red.hit = False  # Hit non-player object state.
+red.antigravity = 0  # The number of "flights" red can make.
+
+blue = Actor('blue_run1')  # Represents "Blue"
+blue.name = 'blue'  # Used to select which colour animation to use.
+blue.pos = (512, 304)  # Start position.
+blue.frame = 3  # Start frame for the animation.
+blue.jumping = False  # Jumping state.
+blue.flying = False  # Flying state.
+blue.kicking = False  # Kicking state.
+blue.landing = False  # Landing state.
+blue.hit = False  # Hit non-player object state.
+blue.antigravity = 0  # The number of "flights" blue can make.
+
+# Floors are drawn one after the other for smooth scrolling.
+floor_a = Actor('floor')
+floor_a.pos = 0, 332
+floor_b = Actor('floor')
+floor_b.pos = 1024, 332
--- paper_chase.py
+++ paper_chase.py
@@ -1,34 +1,10 @@
 """
-Paper Chase.
+Paper Chase -- game logic.
 """
 import random
 from gamedata import *
 
-red = Actor('red_run1')  # Represents "Red"
-red.name = 'red'  # Used to select which colour animation to use.
-red.pos = (256, 304)  # Start position
-red.frame = 1  # Start frame for the animations.
-red.jumping = False  # Jumping state.
-red.flying = False  # Flying state.
-red.kicking = False  # Kicking state.
-red.hit = False  # Hit non-player object state.
-red.antigravity = 0  # The number of "flights" red can make.
-
-blue = Actor('blue_run1')  # Represents "Blue"
-blue.name = 'blue'  # Used to select which colour animation to use.
-blue.pos = (256, 304)  # Start position.
-blue.frame = 3  # Start frame for the animation.
-blue.jumping = False  # Jumping state.
-blue.flying = False  # Flying state.
-blue.kicking = False  # Kicking state.
-blue.hit = False  # Hit non-player object state.
-blue.antigravity = 0  # The number of "flights" blue can make.
-
-# Floors are drawn one after the other for smooth scrolling.
-floor_a = Actor('floor')
-floor_a.pos = 0, 332
-floor_b = Actor('floor')
-floor_b.pos = 1024, 332
+music.play('running_music')  # So sorry... ;-)
 
 def update_player(player):
     """
@@ -50,7 +26,6 @@ def update_player(player):
         if player.frame > 5:
             player.frame = 1
 
-
 def animate_update():
     """
     Update images so we have around 12 FPS.
@@ -59,9 +34,11 @@ def animate_update():
     global speed
     global object_frequency
     global active_objects
-    # Increase difficulty every 200 steps.
+    global power_up
+    global END
+    # Increase difficulty every LEVEL_UP steps.
     steps += 1
-    if steps % 200 == 0:
+    if steps % LEVEL_UP == 0:
         speed = min(40, speed + 4)  # Non plays move faster.
         # Objects appear more frequently.
         object_frequency = max(50, object_frequency - 5)
@@ -78,18 +55,27 @@ def animate_update():
     # Move non-player objects.
     for obj in active_objects:
         obj.left -= speed
+    # Move a power-up
+    if power_up:
+        power_up.left -= speed
+        if power_up.right < 0:
+            power_up = None
     # Check for winning condition
     distance_between_players = abs(red.left - blue.left)
-    if (distance_between_players > 600 or red.right < 0 or 
+    if (distance_between_players > DISTANCE or red.right < 0 or 
             blue.right < 0):
-        endgame()
+        END = True
     else:
         # Re-schedule a call to this function.
         clock.schedule_unique(animate_update, 0.08)
-        
-def endgame():
-    END = True
-    
+
+def toggle_warning():
+    """
+    Used to make the "Steps ahead" info flash.
+    """
+    global WARNING
+    WARNING = not WARNING
+    clock.schedule_unique(toggle_warning, 0.5)
 
 def jump(player, on_finished):
     if not player.flying:
@@ -104,28 +90,30 @@ def fall(player, on_finished):
             on_finished=on_finished, tween='accelerate')
 
 def fly_up(player):
-    x, y = player.pos
-    animate(player, pos=(x, max(20, y - 50)), 
-            duration=0.1, tween='accelerate')
+    if not player.landing:
+        x, y = player.pos
+        animate(player, pos=(x, max(20, y - 50)), 
+                duration=0.1, tween='accelerate')
     
 def fly_down(player, on_land):
-    x, y = player.pos
-    new_y = y + 50
-    if new_y < 290:
-        animate(player, pos=(x, new_y), duration=0.1, 
-                tween='accelerate')
-    else:
-        animate(player, pos=(x, 304), duration=0.1, tween='accelerate')
-        on_land()
+    if not player.landing:
+        x, y = player.pos
+        new_y = y + 50
+        if new_y < 290:
+            animate(player, pos=(x, new_y), duration=0.1, 
+                    tween='accelerate')
+        else:
+            on_land()
 
 def kick(player, on_land):
     player.kicking = True
     clock.schedule_unique(on_land, 0.6)
     
 def land(player, on_land):
+    player.landing = True
     x, y = player.pos
-    animate(player, pos=(x, 304), duration=0.2, tween='accelerate')
-    on_land()
+    animate(player, pos=(x, 304), duration=0.1, tween='accelerate',
+            on_finished=on_land)
 
 def red_land():
     land(red, red_reset)
@@ -134,25 +122,14 @@ def red_reset():
     red.jumping = False
     red.flying = False
     red.kicking = False
+    red.landing = False
 
 def red_jump():
     jump(red, red_fall)
 
 def red_fall():
     fall(red, red_reset)
-    
-def red_fly_up():
-    fly_up(red)
-    if not red.flying:
-        red.flying = True
-        clock.schedule_unique(red_land, FLIGHT_TIME)
-
-def red_fly_down():
-    fly_down(red, red_land)
-    
-def red_kick():
-    kick(red, red_reset)
-
+ 
 def blue_land():
     land(blue, blue_reset)
     
@@ -166,44 +143,95 @@ def blue_reset():
     blue.jumping = False
     blue.flying = False
     blue.kicking = False
-    
-def blue_fly_up():
-    fly_up(blue)
-    if not blue.flying:
-        blue.flying = True
-        clock.schedule_unique(blue_land, FLIGHT_TIME)
-
-def blue_fly_down():
-    fly_down(blue, blue_land)
-    
-def blue_kick():
-    kick(blue, blue_reset)
+    blue.landing = False
 
 def update():
     """
     Update game state in light of user input.
     """
+    if END:  # The race has finished
+        update_end()
+    elif STARTED:  # The race has started
+        update_race()
+    else:  # Just display the intro screen
+        update_intro()
+
+def update_intro():
+    """
+    Wait for the space bar to be pressed and then start
+    the race.
+    """
+    global STARTED
+    if keyboard[keys.SPACE]:
+        STARTED = True
+        # Start the race.
+        clock.schedule_unique(animate_update, 0.08)
+
+def update_end():
+    """
+    Wait for the space bar to be pressed and then reset the
+    game to the start state.
+    """
+    global STARTED
+    global END
+    global speed
+    global object_frequency
+    global steps
     global active_objects
+    if keyboard[keys.SPACE]:
+        STARTED = True
+        END = False
+        speed = 20  # How fast non-player objects move.
+        object_frequency = 100  # Smaller = more frequent.
+        steps = 0
+        red.pos = (512, 304)
+        blue.pos = (512, 304)
+        red.flying = False
+        blue.flying = False
+        red.jumping = False
+        blue.jumping = False
+        red.antigravity = 0
+        blue.antigravity = 0
+        active_objects = []
+        # Start the race.
+        clock.schedule_unique(animate_update, 0.08)
+    
+def update_race():
+    """
+    Update game state when the players are racing.
+    """
+    global active_objects
+    global power_up
     # RED
     if keyboard[keys.RETURN] and not red.jumping:
         red_jump()
     if keyboard[keys.UP] and not red.jumping:
-        red_fly_up()
+        if red.antigravity > 0 and not red.flying:
+            red.antigravity -= 1
+            red.flying = True
+            clock.schedule_unique(red_land, FLIGHT_TIME)
+        if red.flying:
+            fly_up(red)
     if keyboard[keys.DOWN]:
-        red_fly_down()
+        fly_down(red, red_land)
     if (keyboard[keys.RIGHT] and not red.kicking and 
             not red.flying):
-        red_kick()
+        kick(red, red_reset)
     # Blue
     if keyboard[keys.SPACE] and not blue.jumping:
         blue_jump()
     if keyboard[keys.W] and not blue.jumping:
-        blue_fly_up()
+        if blue.antigravity > 0 and not blue.flying:
+            blue.antigravity -= 1
+            blue.flying = True
+            clock.schedule_unique(blue_land, FLIGHT_TIME)
+        if blue.flying:
+            fly_up(blue)
     if keyboard[keys.S]:
-        blue_fly_down()
+        fly_down(blue, blue_land)
     if (keyboard[keys.D] and not blue.kicking and 
             not blue.flying):
-        blue_kick()
+        kick(blue, blue_reset)
     # Check for collisions between players and non-player objects.
     for obj in active_objects:
         # The object has scrolled past.
@@ -234,27 +262,139 @@ def update():
                 obj.blue_hit = True
                 if blue.flying:
                     blue_land()
-    if random.randint(0, object_frequency) == 0:
-        obj_collection = objects[random.choice(list(objects.keys()))]    
-        low = obj_collection['pos'][0]
-        high = obj_collection['pos'][1]
-        new_object = Actor(random.choice(obj_collection['items']), 
-                           pos=(1024, random.randint(low, high)))
-        new_object.red_hit = False
-        new_object.blue_hit = False
-        active_objects.append(new_object)
+    # Check for collision with power-up.
+    if power_up:
+        # This may seem convoluted, but it ensures that if both players get to the
+        # power-up at the same time the game is "fair" in balancing up the advantage.
+        touching_red = (red.colliderect(power_up) and not (red.flying or red.kicking)
+                        and red.antigravity < 3)
+        touching_blue = (blue.colliderect(power_up) and not (blue.flying or blue.kicking)
+                         and blue.antigravity < 3)
+        if touching_blue and touching_red:
+            if red.antigravity > blue.antigravity:
+                blue.antigravity += 1
+            elif red.antigravity < blue.antigravity:
+                red.antigravity += 1
+            else:
+                if random.choice([True, False]):
+                    red.antigravity += 1
+                else:
+                    blue.antigravity += 1
+            power_up = None
+        elif touching_red:
+            red.antigravity += 1
+            power_up = None
+        elif touching_blue:
+            blue.antigravity += 1
+            power_up = None
+    if random.randint(0, object_frequency) == 0 or not active_objects:
+        make_obstacle(ground_objects)
+    if random.randint(0, object_frequency) == 0 or not active_objects:
+        make_obstacle(air_objects)
+    if not power_up and random.randint(0, powerup_frequency) == 0:
+        power_up = Actor('antigravity', pos=(1024, 320))
+
+def make_obstacle(objects):
+    global active_objects
+    obj_collection = objects[random.choice(list(objects.keys()))]    
+    low = obj_collection['pos'][0]
+    high = obj_collection['pos'][1]
+    new_object = Actor(random.choice(obj_collection['items']), 
+                       pos=(1024, random.randint(low, high)))
+    new_object.red_hit = False
+    new_object.blue_hit = False
+    active_objects.append(new_object)
 
 def draw():
     """
     Draw things on the screen.
     """
     screen.blit('paper', (0, 0))
+    if END:  # The race has finished
+        draw_end()
+    elif STARTED:  # The race has started
+        draw_race()
+    else:  # Just display the intro screen
+        draw_intro()
+
+def draw_intro():
+    """
+    Draw the intro screen with the story, keys and instructions
+    to press space to start the race.    
+    """
+    # Paper
+    screen.draw.text('Paper', (260, 10),
+                     fontname='funsized', fontsize=56,
+                     color=(0, 0, 255), background='None')
+    # Chase
+    screen.draw.text('Chase', (500, 10),
+                     fontname='funsized', fontsize=56,
+                     color=(255, 0, 0), background='None')
+    # Story
+    story = ("The intergalactic war between the red and blue factions "
+             "of the biro universe has reached its climax. Each world has "
+             "sent a stick-figure champion to race in the \"Paper chase\" "
+             "for ultimate victory and to decide which colour biro pen "
+             "teachers should use when marking work. (Get {} steps ahead "
+             "to win, collect up to 3 Python power-ups to import antigravity and "
+             "avoid all other obstacles.)").format(DISTANCE)
+    screen.draw.text(story, (50, 80), width=900,
+                     fontname='rudiment', fontsize=30,
+                     color=(0, 0, 0))
+    screen.draw.text('W - fly up, S - fly down\nD - kick object, SPACE - jump.', (50, 240),
+                     fontname='rudiment', fontsize=30,
+                     color=(0, 0, 255))
+    screen.draw.text('Up Arrow - fly up, Down Arrow - fly down\nRight Arrow - kick object, Enter - jump.', (500, 240),
+                     fontname='rudiment', fontsize=30,
+                     color=(255, 0, 0))                 
+    screen.draw.text('Press SPACE to start the race.', (270, 320),
+                     fontname='rudiment', fontsize=38,
+                     color=(0, 0, 0), background='None')
+
+
+def draw_end():
+    """
+    Draw the end state with the result and instructions to
+    press space to start again.
+    """
+    winner = 'Red' if red.left > blue.left else 'Blue'
+    color = (255, 0, 0) if red.left > blue.left else (0, 0, 255)
+    screen.draw.text('{} won!'.format(winner), (360, 100),
+                     fontname='funsized', fontsize=56,
+                     color=color, background='None')
+    screen.draw.text('Press SPACE to restart.', (360, 250),
+                     fontname='rudiment', fontsize=38,
+                     color=(0, 0, 0), background='None')
+    
+def draw_race():
+    """
+    Draw game state when players are racing.
+    """
     red.draw()
     blue.draw()
     floor_a.draw()
     floor_b.draw()
     for obj in active_objects:
         obj.draw()
+    if power_up:
+        power_up.draw()
+    screen.draw.text('Antigravity: {}'.format(red.antigravity),
+                     (800, 340), fontname='rudiment', fontsize=38,
+                     color=(255, 0, 0), background='None')
+    screen.draw.text('Antigravity: {}'.format(blue.antigravity),
+                     (580, 340), fontname='rudiment', fontsize=38,
+                     color=(0, 0, 255), background='None')
+    distance_between_players = int(abs(red.left - blue.left))
+    distance_to_display = distance_between_players - (distance_between_players % 10)
+    color = (255, 0, 0) if red.left > blue.left else (0, 0, 255)
+    alert_margin = int((DISTANCE / 4) * 3)
+    if distance_to_display < alert_margin:
+        screen.draw.text('Steps ahead: {}'.format(distance_to_display),
+                         (10, 340), fontname='rudiment', fontsize=38,
+                         color=color, background='None')
+    elif WARNING:
+        screen.draw.text('Steps ahead: {}'.format(distance_to_display),
+                         (10, 340), fontname='rudiment', fontsize=38,
+                         color=color, background='None')
 
-# Start the race.
-clock.schedule_unique(animate_update, 0.08)
+toggle_warning()
